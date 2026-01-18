08498e1f926802b8c245d1e0006ec63790205ecf
minghinshi <mhshi9768@gmail.com>
2026-01-18 17:46:55 +0800

    Prepare to deploy

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "minghinshi's Resources",
   description: "Blog by minghinshi",
+  base: "/personal-website/",
   lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
