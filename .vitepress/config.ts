84d256cdb3c5e44b439867891d5daafb08cfa162
minghinshi <mhshi9768@gmail.com>
2026-01-18 16:34:41 +0800

    Remove default code

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,23 +6,8 @@ export default defineConfig({
   description: "Blog by minghinshi",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" },
-    ],
+    nav: [{ text: "Home", link: "/" }],
 
-    sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-    ],
-
-    socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
-    ],
+    sidebar: [],
   },
 });
